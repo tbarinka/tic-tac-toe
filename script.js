83d1952f64db6febd4864c7b39c05aeb7afc2715
Taylor Barinka <barinkataylor@gmail.com>
2022-09-08 16:27:29 -0500

    add markUpGameBoard() to controller.checkWhoseTurn()

--- script.js
+++ script.js
@@ -50,23 +50,37 @@ function checkOMarker(marker) {
     return marker == ("O");
 }
 
+function markUpGameBoard() {
+    if (board.grid.at(0) !== "") {cell0.textContent = board.grid.at(0);}
+    if (board.grid.at(1) !== "") {cell1.textContent = board.grid.at(1);}
+    if (board.grid.at(2) !== "") {cell2.textContent = board.grid.at(2);}
+    if (board.grid.at(3) !== "") {cell3.textContent = board.grid.at(3);}
+    if (board.grid.at(4) !== "") {cell4.textContent = board.grid.at(4);}
+    if (board.grid.at(5) !== "") {cell5.textContent = board.grid.at(5);}
+    if (board.grid.at(6) !== "") {cell6.textContent = board.grid.at(6);}
+    if (board.grid.at(7) !== "") {cell7.textContent = board.grid.at(7);}
+    if (board.grid.at(8) !== "") {cell8.textContent = board.grid.at(8);}
+} 
+
 const controller = (() => {
     const checkWhoseTurn = () => {
         if ((board.grid.filter(checkXMarker).length % 2 == 0) && (board.grid.filter(checkOMarker).length % 2 == 1)) {
             let m = prompt("Pick your O move.");
             oPlayer.move(m);
+            markUpGameBoard();
         } else if (board.grid.filter(checkXMarker).length % 2 == 0) {
             let m = prompt("Pick your X move.");
             xPlayer.move(m);
+            markUpGameBoard();
         } else if ((board.grid.filter(checkXMarker).length % 2 == 1) && (board.grid.filter(checkOMarker).length % 2 == 0)) {
             let m = prompt("Pick your O move.");
             oPlayer.move(m);
+            markUpGameBoard();
         } else if ((board.grid.filter(checkXMarker).length % 2 == 1) && (board.grid.filter(checkOMarker).length % 2 == 1)) {
             let m = prompt("Pick your X move.");
             xPlayer.move(m);
+            markUpGameBoard();
         }
     }
-    
     return {checkWhoseTurn}
-
 })();
