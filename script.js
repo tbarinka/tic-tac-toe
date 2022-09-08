3a08da17f5b72d30063e229eb6b4716e74b1a522
Taylor Barinka <barinkataylor@gmail.com>
2022-09-08 16:20:28 -0500

    create controller with checkWhoseTurn method

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ const winnerInfo = document.getElementById("winner-info")
 const turnInfo = document.getElementById("turn-info");
 const reset = document.getElementById("reset");
 
+
+//flow control module
+
+
+//board module
 const board = (() => {
     const grid = ["", "", "", "", "", "", "", "", ""];
     return {
@@ -22,14 +27,46 @@ const board = (() => {
     };
 })();
 
-const Player = (Marker) => {
-    const name = ()) => {
-        prompt("What is your name?");
+//Player factory function
+const Player = (marker) => {
+    const name = (string) => {
+        let name = string;
+        return name;
+    }
+    const mark = marker;
+    const move = (n) => {
+        board.grid.splice(n, 1, marker);
     }
 
-    const marker = Marker;
-
-    return {name, marker}
+    return {name, mark, move}
 }
 const oPlayer = Player("O");
 const xPlayer = Player("X");
+
+function checkXMarker(marker) {
+    return marker == ("X");
+}
+function checkOMarker(marker) {
+    return marker == ("O");
+}
+
+const controller = (() => {
+    const checkWhoseTurn = () => {
+        if ((board.grid.filter(checkXMarker).length % 2 == 0) && (board.grid.filter(checkOMarker).length % 2 == 1)) {
+            let m = prompt("Pick your O move.");
+            oPlayer.move(m);
+        } else if (board.grid.filter(checkXMarker).length % 2 == 0) {
+            let m = prompt("Pick your X move.");
+            xPlayer.move(m);
+        } else if ((board.grid.filter(checkXMarker).length % 2 == 1) && (board.grid.filter(checkOMarker).length % 2 == 0)) {
+            let m = prompt("Pick your O move.");
+            oPlayer.move(m);
+        } else if ((board.grid.filter(checkXMarker).length % 2 == 1) && (board.grid.filter(checkOMarker).length % 2 == 1)) {
+            let m = prompt("Pick your X move.");
+            xPlayer.move(m);
+        }
+    }
+    
+    return {checkWhoseTurn}
+
+})();
